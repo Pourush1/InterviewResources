1f2a5bfaa124d425f754ad15ec25377c2e774e93
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2022-01-13 22:47:30 -0800

    add implementation of stack using linked list

---
+++ src/dataStructures/Stack/stackUsingLinkedList.js
@@ -0,0 +1,51 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    let newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const holdingPointer = this.top;
+      this.top = newNode;
+      this.top.next = holdingPointer;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (this.length === 0) {
+      return null;
+    }
+    if (this.top === this.bottom) {
+      this.bottom = null;
+    }
+    const holdingPointer = this.top;
+    this.top = this.top.next;
+    this.length--;
+    return this;
+  }
+
+  peek() {
+    return this.top;
+  }
+}
+
+let myStack = new Stack();
+myStack.push('Google');
+myStack.push('Udemy');
+myStack.push('Youtube');
+myStack.peek();
